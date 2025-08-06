ed133bae8f24e298baa2d646b489b461cb5aa95b
Gustavo Fernandes <84131142+GusFer-cmd@users.noreply.github.com>
2025-08-06 20:36:58 -0300

    :sparkles: feat: Implementando Breeze

--- fadat-integracao-ava/resources/views/app.blade.php
+++ fadat-integracao-ava/resources/views/app.blade.php
@@ -4,14 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Fadat Ava Integração</title>
-        @inertiaHead
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <body class="font-sans antialiased">
         @inertia
     </body>
 </html>
