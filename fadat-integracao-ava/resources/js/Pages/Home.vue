9791b73547a74f20987484539604f5500d41ebb8
Gustavo Fernandes <84131142+GusFer-cmd@users.noreply.github.com>
2025-08-07 20:12:26 -0300

    :sparkles: feat: Verificação de E-mail

--- fadat-integracao-ava/resources/js/Pages/Home.vue
+++ fadat-integracao-ava/resources/js/Pages/Home.vue
@@ -6,11 +6,11 @@ import { Head } from '@inertiajs/vue3';
 <template>
     <Head title="Home" />
     <AuthenticatedLayout>
-        <template #header>
+        <!-- <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
                 Home
             </h2>
-        </template>
+        </template> -->
 
          <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
